7550dd092f6205824fef673f8a5f3ceb51051710
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-04-17 10:45:36 -0300

    Update ESG screen with new icons in ESGComponents and ESGCircle components

--- src/components/ESGCircle/index.jsx
+++ src/components/ESGCircle/index.jsx
@@ -5,10 +5,11 @@ import React from 'react';
 import styles from './styles';
 import { View, Text, TouchableOpacity } from 'react-native';
 
-const ESGCircle = ({ color, text, style, route }) => {
+const ESGCircle = ({ color, text, style, route, icon }) => {
     return (
         <View style={styles.componentContainer}>
         <TouchableOpacity style={[styles.circle, style, {backgroundColor: color}]}  onPress={() => navigation.navigate(`${route}`, {})}>
+          <Text style={styles.text}>{icon}</Text>
           <Text style={styles.text}>{text}</Text>
         </TouchableOpacity>
         <Text style={styles.textBelowCircle}>ESG</Text>
