1501153d171431bff5c38287081e653c2362b71c
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-04-17 07:49:56 -0300

    Add ESG screen and styles

---
+++ src/screens/ESG/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import Title from '../../components/Title'
+import styles from './styles'
+
+export default function ESG() {
+  return (
+    <View style={styles.mainContainer}>
+        <Title title="ESG" />
+        
+    </View>
+  )
+}
