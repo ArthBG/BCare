d8cca98d8b300059a610241eec6c96d3b06e9baa
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-04-17 08:25:12 -0300

    Update ESG screen with ESG information and navigation options

--- src/screens/ESG/index.jsx
+++ src/screens/ESG/index.jsx
@@ -1,18 +1,56 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import Title from '../../components/Title'
 import styles from './styles'
+import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
+
 
 export default function ESG() {
+
+  const navigation = useNavigation()
+
+
+
   return (
     <View style={styles.mainContainer}>
-        <Title title="ESG" />
-        <Text style={styles.title}>Governança ambiental, social e corporativa.</Text>
+      <Title title="ESG" />
+      <Text style={styles.title}>Governança ambiental, social e corporativa.</Text>
 
+      <ScrollView style={styles.infoContainer}>
         <View style={styles.infoContainer}>
-            <Text style={styles.subtitle}>O que é ESG?</Text>
-            <Text style={styles.text}>ESG é a sigla para Environmental, Social and Governance, que em português significa Ambiental, Social e Governança. ESG é um conjunto de práticas que as empresas adotam para atender a critérios ambientais, sociais e de governança corporativa.</Text>
+          <Text style={styles.subtitle}>O que é ESG?</Text>
+          <Text style={styles.text}>ESG é a sigla para Environmental, Social and Governance, que em português significa Ambiental, Social e Governança. ESG é um conjunto de práticas que as empresas adotam para atender a critérios ambientais, sociais e de governança corporativa.</Text>
         </View>
 
+        <View style={styles.infoContainer}>
+          <Text style={styles.subtitle}>ESG na BCare</Text>
+          <Text style={styles.text}>Na BCare, acreditamos que a sustentabilidade é um fator fundamental para o sucesso de qualquer negócio. Por isso, adotamos práticas de ESG em todas as nossas operações, garantindo que nossas ações tenham um impacto positivo no meio ambiente, na sociedade e na governança corporativa.</Text>
+
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.subtitle}>Veja os detalhes do ESG abaixo:</Text>
+
+          <View style={styles.navigationContainer}>
+
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("sla", {})}>
+              <Text style={styles.text}>Governança ambiental</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("sla", {})}>
+              <Text style={styles.text}>social</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("sla", {})}>
+              <Text style={styles.text}>corporativa</Text>
+            </TouchableOpacity>
+
+          </View>
+
+        </View>
+
+      </ScrollView>
 
     </View>
   )
