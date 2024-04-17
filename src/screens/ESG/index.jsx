7953ebd65b1b3eb00cf74a46e0a5a08201669f26
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-04-17 07:59:32 -0300

    Update ESG screen with ESG information

--- src/screens/ESG/index.jsx
+++ src/screens/ESG/index.jsx
@@ -6,7 +6,14 @@ export default function ESG() {
   return (
     <View style={styles.mainContainer}>
         <Title title="ESG" />
-        
+        <Text style={styles.title}>Governança ambiental, social e corporativa.</Text>
+
+        <View style={styles.infoContainer}>
+            <Text style={styles.subtitle}>O que é ESG?</Text>
+            <Text style={styles.text}>ESG é a sigla para Environmental, Social and Governance, que em português significa Ambiental, Social e Governança. ESG é um conjunto de práticas que as empresas adotam para atender a critérios ambientais, sociais e de governança corporativa.</Text>
+        </View>
+
+
     </View>
   )
 }
