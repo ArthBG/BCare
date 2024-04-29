59b8313ecec3d6b91bd33b056a57877becc961b9
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-04-29 15:58:55 -0300

    Refactor ESGAccessory screen to add Slider component

--- src/screens/ESGAccessory/index.jsx
+++ src/screens/ESGAccessory/index.jsx
@@ -3,7 +3,8 @@ import { Canvas } from '@react-three/fiber/native';
 import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import useControls from "r3f-native-orbitcontrols"
 import { ModelClinic } from '../../components/ModelClinic';
-import ToggleSwitch from '../../components/ToggleSwitch'
+import ToggleSwitch from '../../components/ToggleSwitch';
+import Slider from '@react-native-community/slider';
 
 import styles from './styles'
 
@@ -11,12 +12,12 @@ export default function ESGAccessory() {
   const [OrbitControls, events] = useControls();
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-  
+
   return (
     <View style={styles.containerPage}>
       <View {...events} style={styles.containerModel}>
         <Canvas>
-          <OrbitControls enablePan={false} enableZoom={false} minDistance={1000}  />
+          <OrbitControls enablePan={false} enableZoom={false} minDistance={1000} />
           <directionalLight position={[0, 5, 5]} args={['#fff', 2]} />
           <Suspense fallback={null}>
             <ModelClinic />
@@ -24,9 +25,17 @@ export default function ESGAccessory() {
         </Canvas>
       </View>
 
+      <Slider
+        style={{ width: 400, height: 40 }}
+        minimumValue={0}
+        maximumValue={1}
+        minimumTrackTintColor="#3e3e3e"
+        maximumTrackTintColor="#000000"
+        thumbTintColor="#bb0000"
+      />
 
       <View style={styles.container}>
-        <ToggleSwitch isEnabled={isEnabled} toggleSwitch={toggleSwitch}/>
+        <ToggleSwitch isEnabled={isEnabled} toggleSwitch={toggleSwitch} />
         <Text style={styles.text}>Luz</Text>
       </View>
     </View>
