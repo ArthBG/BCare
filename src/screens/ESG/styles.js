af1714d1403aab2206ab3403e9bedb400d34bef2
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-05-04 22:40:27 -0300

    Update background color in ESG, Home, ScheduleForm, and ScheduleList screens

--- src/screens/ESG/styles.js
+++ src/screens/ESG/styles.js
@@ -5,6 +5,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: '#EAEAEA',
     },
     title: {
         fontSize: 20,
--- src/screens/ScheduleList/styles.js
+++ src/screens/ScheduleList/styles.js
@@ -4,7 +4,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "#fff",
+    backgroundColor: '#EAEAEA',
   },
   scheduleContainer: {
     width: "100%",
