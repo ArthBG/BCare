6d7a59bb3dd1ebb7c560394509f0293b305299bb
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-05-07 21:53:48 -0300

    Createad content in page the ESG Social

--- src/screens/Social/index.jsx
+++ src/screens/Social/index.jsx
@@ -1,14 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import styles from './styles'
 
 
-export default function Social() {
-
+export default function Environmental() {
   return (
-    <View style={styles.mainContainer}>
-      <Text>index</Text>
-    </View>
-  )
+    <ScrollView style={styles.mainContainer}>
+      <Text style={styles.title}>Environmental Clinic</Text>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Compromisso com a Comunidade</Text>
+        <Text style={styles.sectionText}>
+          Na ESG, estamos comprometidos em contribuir positivamente para a comunidade local e promover o bem-estar social.
+        </Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Acesso Equitativo à Saúde</Text>
+        <Text style={styles.sectionText}>
+          Atendimento Inclusivo: Garantimos que nossos serviços sejam acessíveis a todos, independentemente de origem étnica, social ou econômica.
+          Programas de Assistência: Oferecemos programas de assistência e descontos para pacientes em situação de vulnerabilidade.
+        </Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Diversidade e Inclusão</Text>
+        <Text style={styles.sectionText}>
+          Ambiente Inclusivo: Promovemos um ambiente de trabalho diversificado e inclusivo, valorizando a igualdade de oportunidades e respeito mútuo.
+          Cultura de Respeito: Fomentamos uma cultura de respeito e tolerância entre funcionários, pacientes e parceiros.
+        </Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Educação e Conscientização</Text>
+        <Text style={styles.sectionText}>
+          Campanhas de Saúde: Realizamos campanhas educativas sobre saúde preventiva e conscientização sobre doenças crônicas na comunidade.
+          Programas de Treinamento: Oferecemos workshops e palestras educativas abertas à comunidade sobre temas de saúde relevantes.
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>© 2024 Environmental Clinic</Text>
+      </View>
+    </ScrollView>
+  );
 }
 
