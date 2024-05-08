493580c294d2761ad72ee43786817c21bc9a5391
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-05-07 23:05:55 -0300

    feat: image in ESG

--- src/screens/ESG/index.jsx
+++ src/screens/ESG/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
 import Title from '../../components/Title'
 import styles from './styles'
 import { useNavigation } from '@react-navigation/native'
@@ -20,7 +20,10 @@ export default function ESG() {
 
         <Title style={styles.title} title="ESG" />
         <Text style={styles.title}>Governança ambiental, social e corporativa.</Text>
-
+        <Image
+            style={{ width: 320, height: 200, borderRadius: 10, alignSelf: "center", margin: 10 }}
+            source={require("../../../assets/images/ESG.jpg")}
+          />
         <View style={styles.infoContainer}>
           <Text style={styles.subtitle}>O que é ESG?</Text>
           <Text style={styles.text}>ESG é a sigla para Environmental, Social and Governance, que em português significa Ambiental, Social e Governança. ESG é um conjunto de práticas que as empresas adotam para atender a critérios ambientais, sociais e de governança corporativa.</Text>
